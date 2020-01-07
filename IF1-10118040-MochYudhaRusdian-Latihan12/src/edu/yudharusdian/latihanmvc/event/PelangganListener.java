7e0ebedb3d5cdc220b81ba690186cad13257d3a2
Moch Yudha Rusdian <55824602+Yudharus@users.noreply.github.com>
2020-01-07 23:44:49 +0700

    Add files via upload

---
+++ IF1-10118040-MochYudhaRusdian-Latihan12/src/edu/yudharusdian/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.yudharusdian.latihanmvc.event;
+
+import edu.yudharusdian.latihanmvc.model.PelangganModel;
+
+/**
+ *
+ * @author Moch Yudha Rusdian
+ */
+public interface PelangganListener {
+    
+    public void onChange(PelangganModel pelanggan);
+}
---
+++ IF1-10118040-MochYudhaRusdian-Latihan12/src/edu/yudharusdian/latihanmvc/model/IF110118032GunturMaulanaILatihan12.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.yudharusdian.latihanmvc.model;
+
+import edu.yudharusdian.latihanmvc.view.PlangganView;
+
+/**
+ *
+ * @author Moch Yudha Rusdian
+ */
+public class IF110118032GunturMaulanaILatihan12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        PlangganView view = new PlangganView();
+        view.setVisible(true);
+        
+    }
+    
+}
